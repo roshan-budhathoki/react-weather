71a6738d5807e96905550ae6f60b7131690cad40
Roshan Budhathoki <47130986+Quicker123@users.noreply.github.com>
2022-06-17 14:18:07 +0545

    ok ok

--- src/component/preventUser.js
+++ src/component/preventUser.js
@@ -3,9 +3,12 @@ import { Navigate } from 'react-router-dom';
 
 const PreventUser = ({children}) => {
     const userDetails = JSON.parse(localStorage.getItem("userDetails"));
-    if(userDetails?.login){
-        return <Navigate to="/dashboard" replace />;
+    if(userDetails){
+        if(userDetails.login){
+            return <Navigate to="/dashboard" replace />;
+        }
     }
+    
     return children;
 }
 
--- src/component/protectUser.js
+++ src/component/protectUser.js
@@ -3,9 +3,13 @@ import { Navigate } from 'react-router-dom';
 
 const ProtectUser = ({children}) => {
     const userDetails = JSON.parse(localStorage.getItem("userDetails"));
-    if(!userDetails?.login){
-        return <Navigate to="/" replace/>
+    if(userDetails)
+    {
+        if(!userDetails.login){
+            return <Navigate to="/" replace/>
+        }
     }
+    
     return children;
 }
 
